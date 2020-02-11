0241e62818817537016b82367250bffb992d4b33
Jayson Salillas <salillasjayson@gmail.com>
2020-02-11 14:07:13 +0800

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,9 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        "api",
-        "api/*",
-        "http://test.infarmsolutions.com/api",
-        "/api/*",
+        "*",
     ];
 }
