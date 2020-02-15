bdac7b6a8c9fdb6deca9301e98158c373d35c2d3
Jayson Salillas <salillasjayson@gmail.com>
2020-02-15 10:09:47 +0800

    updates

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,7 +42,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
+        //$this->mapApiRoutes();
 
         $this->mapWebRoutes();
 
@@ -72,9 +72,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        // Route::prefix('api')
-        //      ->middleware('api')
-        //      ->namespace($this->namespace)
-        //      ->group(base_path('routes/api.php'));
+        Route::prefix('api')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
     }
 }
