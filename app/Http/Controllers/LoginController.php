0de4947271150c9abd01d1bc2c4e7755973fee35
Jayson Salillas <salillasjayson@gmail.com>
2020-02-19 19:52:39 +0800

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Input;
 use Validator;
-use App\Http\Controllers\Auth;
+use Auth;
 use App\User;
 
 class LoginController extends Controller
