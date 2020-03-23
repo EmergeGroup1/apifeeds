b7b9894ea4667764dce4cacf4006223f7a3eca8f
Jayson Salillas <salillasjayson@gmail.com>
2020-03-23 17:21:51 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6123,7 +6123,7 @@ class HomeController extends Controller
 
 			// if update not exists, update data based on 0 consumption update
 			$history = $this->historyFinder($date_today,$bin_id);
-			if(!empty($history[0]) || $history != NULL){
+			if($history != NULL){
 				if($history[0]['consumption'] == 0.0){
 					$this->testUpdateNoConsumption($history,$date_today);
 				}
