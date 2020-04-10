01b7f2ecc666ab5186a8e78cbd3fa593a61cb7da
Jayson Salillas <salillasjayson@gmail.com>
2020-04-10 12:55:59 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5654,14 +5654,16 @@ class HomeController extends Controller
 	*/
 	public function currentBinCapacity($bin_id){
 
-		$data =  DB::table('feeds_bin_history')
-				//->select(DB::raw('round(feeds_bin_history.amount * 2000,0) AS amount'))
-				->select('amount')
-				->where('bin_id',$bin_id)
-				->orderBy('created_at','desc')
-				->first();
+		// $data =  DB::table('feeds_bin_history')
+		// 		//->select(DB::raw('round(feeds_bin_history.amount * 2000,0) AS amount'))
+		// 		->select('amount')
+		// 		->where('bin_id',$bin_id)
+		// 		->orderBy('created_at','desc')
+		// 		->first();
 
-		$r = $data == NULL ? 0 : round($data->amount * 2000,0);
+		$data = Cache::store('file')->get('bins_history_amount_'.$bin_id);
+
+		$r = $data == NULL ? 0 : round($data['amount'] * 2000,0);
 
 		return $r;
 
