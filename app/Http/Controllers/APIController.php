fcf9fa8c36777a9ae8841c5fa4a2c95a49e805da
Jayson Salillas <salillasjayson@gmail.com>
2020-04-22 17:34:28 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2326,7 +2326,7 @@ class APIController extends Controller
                 ->orderBy('death_id','desc')->get();
 
           for($i=0; $i<count($dt); $i++){
-            $dt[$i]['bins_rooms'] = DB::table("feeds_death_tracker")
+            $dt[$i]->bins_rooms = DB::table("feeds_death_tracker")
                                       ->where('unique_id',$dt[$i]->unique_id)
                                       ->get();
           }
