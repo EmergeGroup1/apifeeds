e0ffbf44c3820e83bf775f9df97b94a3c64cdc36
Jayson Salillas <salillasjayson@gmail.com>
2020-06-13 21:49:48 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2931,6 +2931,10 @@ class APIController extends Controller
                 ->where('death_id',$death_id)
                 ->get();
 
+      for($i=0; $i<count($dt); $i++){
+        $dt[$i]->datereadable = date("m-d-Y H:i a", strtotime($dt[$i]->date_time_logs))
+      }
+
       return $dt;
   }
 
