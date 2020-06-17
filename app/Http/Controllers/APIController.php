50be5d93dccbeb2e66034907621075c6d6b4e7da
Jayson Salillas <salillasjayson@gmail.com>
2020-06-17 11:37:20 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1662,6 +1662,8 @@ class APIController extends Controller
           's_farm'    =>  $request->input('s_farm') // selected farm
         );
 
+        return $data;
+
         $am_controller = new AnimalMovementController;
         $am_lists = $am_controller->animalMovementFilterAPI($data);
         unset($am_controller);
