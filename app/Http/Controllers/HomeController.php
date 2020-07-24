2feed73e348c35c425555fa77103d0541f94f239
Jayson Salillas <salillasjayson@gmail.com>
2020-07-24 20:09:31 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3446,7 +3446,7 @@ class HomeController extends Controller
 				});
 
 				$last_manual_update = array(
-					'last_manual_update'	=>	$sorted_bins[0]['last_manual_update']
+					'last_manual_update'	=>	isset($sorted_bins[0]['last_manual_update']) ? $sorted_bins[0]['last_manual_update'] : 0
 				);
 
 				$empty_bins = array(
