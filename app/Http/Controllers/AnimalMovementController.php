12c3bfa0845d9ee2f598c62c35b2207226534fb9
Jayson Salillas <salillasjayson@gmail.com>
2020-07-28 21:17:09 +0800

    Update AnimalMovementController.php

--- app/Http/Controllers/AnimalMovementController.php
+++ app/Http/Controllers/AnimalMovementController.php
@@ -2507,7 +2507,7 @@ class AnimalMovementController extends Controller
           }
 
 
-          $output[] = $farm_name['name'] . " - " . substr($bor_name, 0, -2);
+          $output[] = $groups[$i]->group_name . " " . $farm_name['name'] . " - " . substr($bor_name, 0, -2);
         }
 
         return $output;
