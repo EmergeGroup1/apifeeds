d81c1e518af594c06f12f76159a6866e82047942
Jayson Salillas <salillasjayson@gmail.com>
2020-07-29 17:32:27 +0800

    Update AnimalMovementController.php

--- app/Http/Controllers/AnimalMovementController.php
+++ app/Http/Controllers/AnimalMovementController.php
@@ -2496,7 +2496,7 @@ class AnimalMovementController extends Controller
 
             for($j=0; $j<count($bin_or_rooms); $j++){
 
-              $bins = Bins::where("bin_id",$bin_or_rooms[$j]->bin_id)->first("bin_alias");
+              $bins = Bins::where("bin_id",$bin_or_rooms[$j]->bin_id)->first("alias");
 
               $bor_n .= $bins['alias'] . ", ";
 
