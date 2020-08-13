67b244f1dbe16c534a6edcd4717400aaa1353a89
Jayson Salillas <salillasjayson@gmail.com>
2020-08-13 12:18:42 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3245,7 +3245,7 @@ class APIController extends Controller
                             'group_id'        =>   $data['group_id'],
                             'bin_id'          =>   $t_data[$i]['bin_id'],
                             'room_id'         =>   $t_data[$i]['room_id'],
-                            'date'            =>   $data['group_id'],
+                            'date'            =>   $data['date'],
                             'amount'          =>   $t_data[$i]['amount'],
                             'treatment'       =>   $t_data[$i]['treatment'],
                             'notes'           =>   $t_data[$i]['notes']
@@ -3256,7 +3256,7 @@ class APIController extends Controller
                           $treated_data[$u_comb_bor_keys[$i]] = array(
                             'group_id'        =>   $data['group_id'],
                             'bin_id'          =>   $t_data[$i]['bin_id'],
-                            'room_id'         =>   $t_data[$i]['room_id'],
+                            'room_id'         =>   $data['date'],
                             'date'            =>   $data['group_id'],
                             'amount'          =>   $t_data[$i]['amount'],
                             'treatment'       =>   $t_data[$i]['treatment'],
