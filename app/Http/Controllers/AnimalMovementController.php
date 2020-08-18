c8cbabed3a2a1544ec18eb0f83d0f9e37e5b8084
Jayson Salillas <salillasjayson@gmail.com>
2020-08-18 15:38:04 +0800

    Update AnimalMovementController.php

--- app/Http/Controllers/AnimalMovementController.php
+++ app/Http/Controllers/AnimalMovementController.php
@@ -688,7 +688,8 @@ class AnimalMovementController extends Controller
 
             if($transfer_data != NULL){
 
-              $date_to_transfer = date('Y-m-d',strtotime($transfer_data[0]['date_ymd']. ' + 15 days'));
+              // $date_to_transfer = date('Y-m-d',strtotime($transfer_data[0]['date_ymd']. ' + 15 days'));
+              $date_to_transfer = (strtotime(date('Y-m-d',strtotime($transfer_data[0]['date_ymd']. ' + 15 days'))) - strtotime(date('Y-m-d'))) / (60 * 60 * 24)
               $days_remaining = idate('y', strtotime($date_to_transfer));
 
             }
