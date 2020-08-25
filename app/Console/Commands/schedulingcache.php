0dcfdf4446e85df42b0082ec4e0c07e58c58097a
Jayson Salillas <salillasjayson@gmail.com>
2020-08-26 04:57:17 +0800

    Update schedulingcache.php

--- app/Console/Commands/schedulingcache.php
+++ app/Console/Commands/schedulingcache.php
@@ -52,5 +52,21 @@ class schedulingcache extends Command
       echo $output;
       // close curl resource to free up system resources
       curl_close($ch);
+
+
+      // create curl resource
+      $ch = curl_init();
+
+      // set url
+      curl_setopt($ch, CURLOPT_URL, 'http://'.env('APP_DOMAIN').'/pigtrackerdata');
+
+      //return the transfer as a string
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+      // $output contains the output string
+      $output = curl_exec($ch);
+      echo $output;
+      // close curl resource to free up system resources
+      curl_close($ch);
     }
 }
