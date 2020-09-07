94b980144d809f4f86cee8774f80ebbe332eeb91
Jayson Salillas <salillasjayson@gmail.com>
2020-09-07 20:34:27 +0800

    Update FarmsController.php

--- app/Http/Controllers/FarmsController.php
+++ app/Http/Controllers/FarmsController.php
@@ -1718,7 +1718,7 @@ class FarmsController extends Controller
         return array(
           'counter_one' => $counter_one,
           'counter_two' => ($counter_one + $counter_one) + 1,
-          'counter_three' => ($counter_one + $counter_one + $counter_one) - 1;
+          'counter_three' => ($counter_one + $counter_one + $counter_one) - 1
         );
 
       }
