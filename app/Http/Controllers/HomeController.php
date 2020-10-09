37c877c79682cccd85fe7f0f42045ca75932d7e1
Jayson Salillas <salillasjayson@gmail.com>
2020-10-09 19:55:36 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -81,7 +81,6 @@ class HomeController extends Controller
 
 				$farms = $this->enabledFarms();
 				$forecastingData = $this->farmsDataBuilder($farms);
-				return $farms;
 
 				// cache data via sort type low bins
 				usort($forecastingData, function($a,$b){
@@ -147,11 +146,9 @@ class HomeController extends Controller
 				for($i=0; $i<count($farms); $i++){
 
 					Cache::forget('farm_holder-'.$farms[$i]['id']);
-					if(isset($farms[$i]['id'])) {
+					if(isset($farms[$i]['id']) && Cache::has('farm_holder-'.$farms[$i]['id'])) {
 
-						if(Cache::has('farm_holder-'.$farms[$i]['id'])) {
 							$forecastingData[] = Cache::get('farm_holder-'.$farms[$i]['id'])[$i];
-						}
 
 					} else {
 						$b_data = $this->binsDataFirstLoadV2($farms[$i]['id'],$farms[$i]['update_notification']);
