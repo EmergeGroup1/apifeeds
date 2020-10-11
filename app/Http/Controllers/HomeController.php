2237e10cf1bd8146559e92e012cd4a3640e13ce1
Jayson Salillas <salillasjayson@gmail.com>
2020-10-12 07:37:25 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3634,7 +3634,7 @@ class HomeController extends Controller
 
 					}
 
-					$binsDataFinal[$i] = $empty_bins+$days_to_empty_first+$binsData[$i];
+					$binsDataFinal[$i] = $empty_bins+$days_to_empty_first+$binsData;
 				}
 
 				// disabled update notifications
