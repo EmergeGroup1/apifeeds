cd4518ce645ef2953b389a18811264225b1f83a9
Jayson Salillas <salillasjayson@gmail.com>
2020-10-12 16:26:53 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3585,7 +3585,7 @@ class HomeController extends Controller
 					'first_list_days_to_empty'	=>	isset($sorted_bins[0]['days_to_empty']) ? $sorted_bins[0]['days_to_empty'] : 0
 				);
 
-				$sorted_bins = $binsData;
+				$sorted_bins = $binsData[0];
 				usort($sorted_bins, function($a,$b){
 					if(isset($a['last_manual_update'])){
 						if($a['last_manual_update'] == $b['last_manual_update']) return 0;
