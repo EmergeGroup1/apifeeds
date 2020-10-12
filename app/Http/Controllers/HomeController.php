969bdbacdbb7e80558326fa437b07f9cfe6886f1
Jayson Salillas <salillasjayson@gmail.com>
2020-10-12 17:47:55 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3575,7 +3575,7 @@ class HomeController extends Controller
 
 				}
 
-				$sorted_bins = $binsData[0];
+				$sorted_bins = $binsData;
 
 				usort($sorted_bins, function($a,$b){
 					if(isset($a['days_to_empty'])){
