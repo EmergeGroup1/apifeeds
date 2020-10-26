0580d0441ab5c0af58c97c2105c3c13e87c812c5
MarcOrdan <marcordan@gmail.com>
2020-10-26 23:58:51 +0800

    FDLT-691
    Api Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// lIST ALL CME
+Route::get('cmes', 'App\Http\Controllers\CmeController@index');
+
+// LIST SINGLE CME RECORD
+Route::get('cme/{id}', 'App\Http\Controllers\CmeController@show');
+
+// CREATE NEW CME RECORD
+Route::post('cme', 'App\Http\Controllers\CmeController@store');
+
+// UPDATE CME RECORD
+Route::put('cme/{id}', 'App\Http\Controllers\CmeController@store');
+
+//UPDATE VISIBILITY
+Route::put('cme/{id}', 'App\Http\Controllers\CmeController@visibility');
+
+// DELETE CME RECORD
+Route::delete('cme/{id}', 'App\Http\Controllers\CmeController@destroy');
