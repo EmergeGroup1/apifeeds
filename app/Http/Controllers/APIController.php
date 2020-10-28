a72553c7d61fddc0ce3db1b4a734d1b2ba357a6b
Jayson Salillas <salillasjayson@gmail.com>
2020-10-28 19:34:02 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3236,6 +3236,8 @@ class APIController extends Controller
 
           $output = NULL;
 
+          return "testing";
+
           $mo_ctrl = new MarcController;
           $output = $mo_ctrl->testMethod();
           unset($mo_ctrl);
