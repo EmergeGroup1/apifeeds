f71d0ae674a6a1e97085c5561234830e2230d0f9
MarcOrdan <marcordan@gmail.com>
2020-10-30 19:49:09 +0800

    FDLT-693
    Update routes.

--- app/Http/Controllers/CmeController.php
+++ app/Http/Controllers/CmeController.php
@@ -22,11 +22,12 @@ class CmeController extends Controller
    */
   public function index()
   {
-    //GET CME
-    $cmes = Cme::all();
+    // //GET CME
+    // $cmes = Cme::all();
 
-    // RETURN collection of CME as a resource
-    return CmeResource::collection($cmes);
+    // // RETURN collection of CME as a resource
+    // return CmeResource::collection($cmes);
+    return response()->json(CmeResource::get(), 200);
   }
 
   /**
