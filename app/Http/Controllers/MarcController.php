72fcd13c620587a1bdb8945d7bb9bdb4ee31c64c
MarcOrdan <marcordan@gmail.com>
2020-10-30 21:12:15 +0800

    FDLT-693
    Update controller.

--- app/Http/Controllers/MarcController.php
+++ app/Http/Controllers/MarcController.php
@@ -21,70 +21,94 @@ class MarcController extends Controller
 		return response()->json(Cme::get(), 200);
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
+	public function cmeByID($id)
 	{
-		$cme = $request->isMethod('put') ? Cme::findOrFail($request->id) : new Cme;
-
-		$cme->id = $request->input('id');
-		$cme->month = $request->input('month');
-		$cme->last = $request->input('last');
-		$cme->converted_last = $request->input('converted_last');
-		$cme->basis = $request->input('basis');
-		$cme->bidvalue = $request->input('bidvalue');
-		$cme->visibility_ui = $request->input('visibility_ui');
-
-		if ($cme->save()) {
-			return $cme;
-		}
-	}
-
-	// Update Visibility
-	public function visibility(Request $request)
-	{
-		$cme = $request->isMethod('put') ? Cme::findOrFail($request->id) : new Cme;
-
-		$cme->visibility_ui = $request->input('visibility_ui');
-		if ($cme->save()) {
-			return $cme;
-		}
-	}
-
-	/**
-	 * RETURN SINGLE CME RECORD.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id)
-	{
-		// Get CME via id
-		$cme = Cme::findOrFail($id);
-
-		//return single cme record
-		return $cme;
+		return response()->json(Cme::find($id), 200);
 	}
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id)
-	{
-		// Get CME record via id
-		$cme = Cme::FindOrFail($id);
 
-		// Delete single cme record
-		if ($cme->delete()) {
-			return $cme;
-		}
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// /**
+	//  * Store a newly created resource in storage.
+	//  *
+	//  * @param  \Illuminate\Http\Request
+	//  * @return \Illuminate\Http\Response
+	//  */
+	// public function store(Request $request)
+	// {
+	// 	$cme = $request->isMethod('put') ? Cme::findOrFail($request->id) : new Cme;
+
+	// 	$cme->id = $request->input('id');
+	// 	$cme->month = $request->input('month');
+	// 	$cme->last = $request->input('last');
+	// 	$cme->converted_last = $request->input('converted_last');
+	// 	$cme->basis = $request->input('basis');
+	// 	$cme->bidvalue = $request->input('bidvalue');
+	// 	$cme->visibility_ui = $request->input('visibility_ui');
+
+	// 	if ($cme->save()) {
+	// 		return $cme;
+	// 	}
+	// }
+
+	// // Update Visibility
+	// public function visibility(Request $request)
+	// {
+	// 	$cme = $request->isMethod('put') ? Cme::findOrFail($request->id) : new Cme;
+
+	// 	$cme->visibility_ui = $request->input('visibility_ui');
+	// 	if ($cme->save()) {
+	// 		return $cme;
+	// 	}
+	// }
+
+	// /**
+	//  * RETURN SINGLE CME RECORD.
+	//  *
+	//  * @param  int  $id
+	//  * @return \Illuminate\Http\Response
+	//  */
+	// public function show($id)
+	// {
+	// 	// Get CME via id
+	// 	$cme = Cme::findOrFail($id);
+
+	// 	//return single cme record
+	// 	return $cme;
+	// }
+
+
+	// /**
+	//  * Remove the specified resource from storage.
+	//  *
+	//  * @param  int  $id
+	//  * @return \Illuminate\Http\Response
+	//  */
+	// public function destroy($id)
+	// {
+	// 	// Get CME record via id
+	// 	$cme = Cme::FindOrFail($id);
+
+	// 	// Delete single cme record
+	// 	if ($cme->delete()) {
+	// 		return $cme;
+	// 	}
+	// }
 }
