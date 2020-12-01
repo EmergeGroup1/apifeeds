6e7ef10bb32ee4187da5d30f5886f7bf92146b42
Jayson Salillas <salillasjayson@gmail.com>
2020-12-01 18:22:30 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1827,7 +1827,14 @@ class HomeController extends Controller
 							->where("bin_id",$bin_id)
 							->get();
 
-		return $groups;
+		$data = array();
+		for($i=0; $i < count($groups); $i++){
+			$data[] = DB::table("feeds_movement_groups")
+							->where("unique_id",$groups[$i]->unique_id)
+							->get();
+		}
+
+		return $data;
 
 		for($i=0; $i < count($groups); $i++){
 
