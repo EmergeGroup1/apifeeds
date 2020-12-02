eefb1c1d91118c0aae8389c031d1792e386096db
Jayson Salillas <salillasjayson@gmail.com>
2020-12-02 21:52:01 +0800

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3235,9 +3235,10 @@ class APIController extends Controller
           $data = $request->all();
 
           $bin_id = $data['bin_id'];
+          $amount_tons = $data['amount_tons'];
 
           $home_ctrl = new HomeController;
-          $cons = $home_ctrl->updateGroupsConsumption($bin_id);
+          $cons = $home_ctrl->updateGroupsConsumption($bin_id,$amount_tons);
           unset($home_ctrl);
 
           return $cons;
