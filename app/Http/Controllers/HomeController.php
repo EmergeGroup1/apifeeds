55791beec227f62dccfe7a742676af7eeebcedc5
Jayson Salillas <salillasjayson@gmail.com>
2020-12-07 23:54:15 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1886,7 +1886,7 @@ class HomeController extends Controller
 					'total_pigs'	=>	$total_pigs
 				);
 
-				$amount = $amount - ($budgeted_amount_lbs / 2000);
+				$amount = $amount - ($budgeted_amount_lbs * $total_pigs);
 
 		}
 
