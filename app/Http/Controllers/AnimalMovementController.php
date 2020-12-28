613c0c80fbcbbe0869e632f03c466d7472146089
Jayson Salillas <salillasjayson@gmail.com>
2020-12-28 13:07:10 +0800

    Update AnimalMovementController.php

--- app/Http/Controllers/AnimalMovementController.php
+++ app/Http/Controllers/AnimalMovementController.php
@@ -895,6 +895,7 @@ class AnimalMovementController extends Controller
 
         if($type == "nursery"){
 
+          return $transfered_groups->get()->isNotEmpty();
           if($transfered_groups->get()->isNotEmpty()){
 
             $transfered_groups = $transfered_groups->where('group_to',$group_id);
