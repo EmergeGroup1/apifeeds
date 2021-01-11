f70885fa20cbe4fc5973c721e65b125e046bc38e
Jayson Salillas <salillasjayson@gmail.com>
2021-01-12 04:03:33 +0800

    Update AnimalMovementController.php

--- app/Http/Controllers/AnimalMovementController.php
+++ app/Http/Controllers/AnimalMovementController.php
@@ -1151,7 +1151,7 @@ class AnimalMovementController extends Controller
                                     ->where("group_from",$v['group_id'])
                                     ->select("final_count")
                                     ->orderBy('transfer_id','desc')
-                                    ->first('final_count');
+                                    ->first();
 
             if($v['status'] == "removed" || $v['status'] == "finalized"){
 
@@ -1176,7 +1176,7 @@ class AnimalMovementController extends Controller
                   'deceased'					      =>	$this->deceasedPigs($v['group_id']),
                   'treated'						      =>	$this->treatedPigs($v['group_id']),
                   'previous_total_pigs'			=>	$previous_total_pigs,
-                  'current_total_pigs'      =>  $current_total_pigs->final_count,
+                  'current_total_pigs'      =>  $current_total_pigs != NULL ? $current_total_pigs->final_count ? 0,
                   'farm_name'					      =>	$this->farmData($v['farm_id']),
                   'bin_data'					      =>	$this->binsDataFilter($v['unique_id'],$group_bins_table,$v['farm_id']),
                   'transfer_data'			      => 	$this->transferData($v['group_id']),
