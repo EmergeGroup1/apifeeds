3870d1aff2053d5f065aeda6bf7208824447bfb9
Jayson Salillas <salillasjayson@gmail.com>
2021-01-27 20:33:20 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1931,21 +1931,8 @@ class HomeController extends Controller
 			// manual (insert) select the previous data and build the new data to insert
 			if($type == "manual"){
 				if($groups_cons_history->isNotEmpty()){
-
-					if($groups_cons_history[0]->update_date == date('Y-m-d')){
-
-						if($groups_cons_history[0]->actual_amount_tons >= $groups_consumption_data[$i]['actual_amount_tons']){
-							return false;
-						} else {
-							$budgeted_consumption_lbs = $groups_cons_history[0]->budgeted_consumption_lbs;
-							$budgeted_amount_tons = $groups_cons_history[0]->budgeted_amount_tons;
-						}
-
-					} else {
-						$budgeted_consumption_lbs = $groups_cons_history[0]->budgeted_consumption_lbs;
-						$budgeted_amount_tons = $groups_cons_history[0]->budgeted_amount_tons;
-					}
-
+					$budgeted_consumption_lbs = $groups_cons_history[0]->budgeted_consumption_lbs;
+					$budgeted_amount_tons = $groups_cons_history[0]->budgeted_amount_tons;
 				} else {
 					$budgeted_consumption_lbs = 0;
 					$budgeted_amount_tons = 0;
